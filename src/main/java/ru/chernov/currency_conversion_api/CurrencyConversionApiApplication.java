0d1cbc6d1a527ee4d60ce175a1d61700a6c708ef
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-07-16 01:28:46 +0300

    enable feign clients

--- src/main/java/ru/chernov/currency_conversion_api/CurrencyConversionApiApplication.java
+++ src/main/java/ru/chernov/currency_conversion_api/CurrencyConversionApiApplication.java
@@ -2,8 +2,10 @@ package ru.chernov.currency_conversion_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CurrencyConversionApiApplication {
 
 	public static void main(String[] args) {
