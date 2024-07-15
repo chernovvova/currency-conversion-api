fc3802dbde1c54ba78c1f90c15f5b67337567d16
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-07-15 23:06:46 +0300

    Initial commit

---
+++ src/test/java/ru/chernov/currency_conversion_api/CurrencyConversionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.chernov.currency_conversion_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
