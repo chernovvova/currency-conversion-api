621189461f873abfb6eb59ae0b0bf3d2acde4e4f
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-07-16 13:46:28 +0300

    add query that get actual convrersion rate from db

--- src/main/java/ru/chernov/currency_conversion_api/repository/ConversionRateRepository.java
+++ src/main/java/ru/chernov/currency_conversion_api/repository/ConversionRateRepository.java
@@ -1,12 +1,18 @@
 package ru.chernov.currency_conversion_api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import feign.Param;
 import ru.chernov.currency_conversion_api.entity.ConversionRateEntity;
 import ru.chernov.currency_conversion_api.entity.ConversionRateId;
 
 @Repository
 public interface ConversionRateRepository extends JpaRepository<ConversionRateEntity, ConversionRateId>{
-
+    @Query(value = "SELECT * FROM conversion_rate WHERE target_code = :targetCode AND :time BETWEEN time_last_update AND time_next_update", 
+            nativeQuery = true)
+    List<ConversionRateEntity> findByTargetCodeAndTimeInterval(@Param("targetCode")String targetCode, @Param("time") Long time);
 } 
