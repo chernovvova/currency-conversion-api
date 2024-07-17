1803c8705e184ed81db4aee12e6fd5fb68cda103
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-07-17 18:36:14 +0300

    Add swager configuration class

---
+++ src/main/java/ru/chernov/currency_conversion_api/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package ru.chernov.currency_conversion_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI swaggerDescription() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Конвертер валют")
+                        .description("UI конвертера валют")
+                        .version("0.0.1"));
+    }
+}
